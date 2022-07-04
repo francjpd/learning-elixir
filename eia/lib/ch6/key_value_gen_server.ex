0e89c458f0ecbe7d56eec64c0014f3ede36b1dea
francjpd <francjpd@gmail.com>
2022-07-04 23:34:40 +0200

    docs(eia) ch6  finished

---
+++ eia/lib/ch6/key_value_gen_server.ex
@@ -0,0 +1,31 @@
+defmodule KeyValueStoreGenServer do
+  use GenServer
+
+  def start do
+    GenServer.start(__MODULE__, nil, [name: __MODULE__])
+  end
+
+  def put(key, value ) do
+    GenServer.cast(__MODULE__, {:put, key, value})
+  end
+
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  @impl GenServer
+  def init(_) do
+    :timer.send_interval(5000, :cleanup)
+    {:ok, %{}}
+  end
+
+  @impl GenServer
+  def handle_call({:get, key}, _ , state) do
+    {:reply, Map.get(state, key), state}
+  end
+
+  @impl GenServer
+  def handle_cast({:put, key, value}, state) do
+    {:noreply, Map.put(state, key, value)}
+  end
+end
